54a9630528eacdadaba65f8536cc569d774001f3
heyitsmeniraj <56302682+heyitsmeniraj@users.noreply.github.com>
2025-04-21 16:45:43 +0100

    21_04)25 commit
    Idk what has changed haven't been keeping track but might as well push it

--- Assets/scripts/PlaceLights.cs
+++ Assets/scripts/PlaceLights.cs
@@ -7,6 +7,9 @@ public class PlaceLights : MonoBehaviour
     public GameObject seekLightPrefab;
     public Collider[] MazeArea;
     public Collider ExcludeArea;
+    public float offsetZ = 1f;
+
+    Vector3 gizmoPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,14 +30,23 @@ public class PlaceLights : MonoBehaviour
         }
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(gizmoPos - Vector3.up * 1, CheckWallRadius);
+    }
+
     public void PlaceSeekLight()
     {
         if (!finishedPlacing)
         {
             Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 10;
+            mousePos.z = Camera.main.transform.position.z;// - offsetZ;
             Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
             objectPos.y = 1;
+            objectPos.z -= offsetZ;
+
+            gizmoPos = objectPos;
 
             if (ExcludeArea.bounds.Contains(objectPos))
             {
@@ -47,6 +59,7 @@ public class PlaceLights : MonoBehaviour
                 {
                     if (mazeCollider.bounds.Contains(objectPos))
                     {
+
                         Instantiate(seekLightPrefab, objectPos, Quaternion.identity);
                         return;
                     }
--- Assets/scripts/SeekableObjectr.cs
+++ Assets/scripts/SeekableObjectr.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class SeekableObjectr : MonoBehaviour
 {
     public int priority = 1; // Higher values mean higher priority
     public bool visited = false;
+    bool placed = false;
+    public float CheckWallRadius = 2;
+    public LayerMask wallMask;
+    public float wallOffset = 0.1f;
+
+    private void Update()
+    {
+        if (placed) return;
+        CheckPosition();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,5 +24,54 @@ public class SeekableObjectr : MonoBehaviour
             visited = true;
         }
     }
+
+    void CheckPosition()
+    {
+        List<RaycastHit> hits = new List<RaycastHit>();
+        List<Vector3> dirs = new List<Vector3>();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, CheckWallRadius, wallMask))
+        {
+            hits.Add(hit);
+            dirs.Add(transform.forward);
+        }
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, CheckWallRadius, wallMask))
+        {
+            hits.Add(hit);
+            dirs.Add(-transform.forward);
+
+        }
+        if (Physics.Raycast(transform.position, transform.right, out hit, CheckWallRadius, wallMask))
+        {
+            hits.Add(hit);
+            dirs.Add(transform.right);
+
+        }
+        if (Physics.Raycast(transform.position, -transform.right, out hit, CheckWallRadius, wallMask))
+        {
+            hits.Add(hit);
+            dirs.Add(-transform.right);
+
+        }
+
+        float hitDistance = 10000;
+        Vector3 hitDirection = Vector3.zero;
+        for (int i= 0; i < hits.Count; i++)
+        {
+            if (hits[i].distance < hitDistance)
+            {
+                hitDistance = hits[i].distance;
+                hitDirection = dirs[i];
+            }
+        }
+
+        transform.position += hitDirection.normalized * (hitDistance - wallOffset);
+        placed = true;
+    }
 }
 
+//RaycastHit[] hits = Physics.SphereCastAll(objectPos, CheckWallRadius, Vector3.up, 1, wallMask);
+//if (hits.Length > 0) 
+//{
+//    foreach (RaycastHit hit in hits) { print(hit.distance); }
+//}
